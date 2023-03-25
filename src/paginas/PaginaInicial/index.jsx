9f08b5f25fb283c5bbdb07fad0c739232de188d5
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-25 14:26:01 -0300

    Iniciando a criacao do Menu

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,12 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
 
+//Essa Tag aberta e fechando sem nada, é um Fragment, um elemento pai
 export default function PaginaInicial(){
     return(
-        <Cabecalho/>
+        <>
+            <Cabecalho />
+            <Menu />
+        </>
     )
 }
