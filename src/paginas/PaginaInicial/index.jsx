9bffe147917b8931b66cff431438d3315306d642
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-25 14:43:04 -0300

    Adicionando o banner no projeto

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,12 +1,22 @@
+import banner from './banner.png'
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import styles from './PaginaInicial.module.scss'
 
 //Essa Tag aberta e fechando sem nada, é um Fragment, um elemento pai
 export default function PaginaInicial(){
     return(
         <>
             <Cabecalho />
-            <Menu />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
