3d2792ccd314762cdd43105ff18df1b17069fac3
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-29 21:01:36 -0300

    Incluindo o absolute import no componente Menu

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import home from '../../assets/icones/home-ativo.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
-import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
-import novas from '../../assets/icones/novas-inativo.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
+import home from 'assets/icones/home-ativo.png'
+import maisCurtidas from 'assets/icones/mais-curtidas-inativo.png'
+import maisVistas from 'assets/icones/mais-vistas-inativo.png'
+import novas from 'assets/icones/novas-inativo.png'
+import surpreendaMe from 'assets/icones/surpreenda-me-inativo.png'
 import styles from './Menu.module.scss'
 
 export default function Menu() {
